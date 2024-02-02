85db3154a15424ad705ea03b009de03e79151399
Moumen003 <moumenibrahim219@yahoo.com>
2024-02-02 16:09:32 +0200

    sdfzc

--- models/base_model.py
+++ models/base_model.py
@@ -47,7 +47,3 @@ class BaseModel:
         dict_copy["updated_at"] = self.updated_at.isoformat()
         dict_copy['__class__'] = self.__class__.__name__
         return dict_copy
-
-    #@property
-    #def id(self):
-    #    return self.id
